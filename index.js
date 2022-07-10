828a270b9ffde79785ba263b01d58db501f0243e
Matthew Henderson <info@matthewahenderson.com>
2022-07-09 22:02:04 -0400

    Added mongoose dependency; added first model for accessing DB; added callback to passport; refactored routes to separate file

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
-    })
-);
-
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get(
-    '/auth/google/callback',
-    passport.authenticate('google')
-);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
