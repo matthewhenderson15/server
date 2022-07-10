7de8c49fae4206cf65993557eb107d8332ae3ae9
Matthew Henderson <info@matthewahenderson.com>
2022-07-10 02:11:32 -0400

    Added create react app depedency; generated boilerplate for React

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>
+          hi there
+        </h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a href="http://localhost:4000/auth/google">Sign In With Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
