70dbf0663427403e8ca9827c5b394d5242c29fd6
Matthew Henderson <info@matthewahenderson.com>
2022-07-21 00:59:28 -0400

    Added case for fetchUser to auth reducer; added map state to props function to Header class

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
     render() {
+        console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper">
@@ -19,4 +21,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
