d3e3b5046cd16cf98c4e4864cfccb56d5136abd2
Matthew Henderson <info@matthewahenderson.com>
2022-07-24 18:34:50 -0400

    Added styling to Header; added development and production environment files with Stripe keys; added Heroku commands; added support for routing on client side

--- index.js
+++ index.js
@@ -25,5 +25,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
