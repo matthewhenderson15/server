fc37e15de508a47c3ad9bb7e3dc50450c8f47d07
Matthew Henderson <info@matthewahenderson.com>
2022-07-31 15:28:49 -0400

    Added redirect domain to survey template file; added try catch block and async syntax to Mailer class; removed call to click tracking in constructor

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
@@ -21,5 +21,15 @@ module.exports = app => {
 
         const mailer = new Mailer(survey, surveyTemplate(survey));
 
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+        
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -7,13 +7,13 @@ class Mailer extends helper.Mail {
         super();
 
         this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.from_email = new helper.Email('info@matthewahenderson.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
 
         this.addContent(this.body);
-        this.addClickTracking();
+        // this.addClickTracking();
         this.addRecipients();
     }
 
@@ -47,7 +47,7 @@ class Mailer extends helper.Mail {
             body: this.toJSON()
         });
 
-        const response = this.sgApi.API(request);
+        const response = await this.sgApi.API(request);
         return response;
     }
 }
