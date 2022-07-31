ca97d11c3246126f9d24cc83643e880ae6383d15
Matthew Henderson <info@matthewahenderson.com>
2022-07-31 18:01:00 -0400

    Removed console.log from authReducer; added logic to display input and label in SurveyField component; refactored renderFields method and added form styling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,19 +1,39 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
+];
  
 class SurveyForm extends Component {
+    renderFields() {
+        return _.map(FIELDS, ({ label, name }) => {
+            return (
+                <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+            );
+        });
+    }
+    
     render() {
-        return(
+        return (
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-                    <Field 
-                        type="text"
-                        name="surveyTitle"
-                        component="input"
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-                
+                    {this.renderFields()}
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
+                </form> 
             </div>
         );
     }
