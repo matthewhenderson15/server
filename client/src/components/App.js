9eeff179c94f72b759815fe39da6c77a3f8381a4
Matthew Henderson <info@matthewahenderson.com>
2022-07-31 17:03:22 -0400

    Added form reducer to index.js; added new class components for creating new surveys

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
     componentDidMount() {
---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+ 
+class SurveyForm extends Component {
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    <Field 
+                        type="text"
+                        name="surveyTitle"
+                        component="input"
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+                
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyForm);
