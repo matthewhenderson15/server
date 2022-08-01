43dabd1d380637e2940d76bb8320d61c5645e97c
Matthew Henderson <info@matthewahenderson.com>
2022-08-01 19:01:57 -0400

    Added reduxForm helper for dumping form values; added action for submitting the survey

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,5 +10,9 @@ export const fetchUser = () => async dispatch => {
 export const handleToken = token => async dispatch => {
     const res = await axios.post('/api/stripe', token);
 
-    dispatch({ type: FETCH_USER, payload: res.data })
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = values => {
+    return { type: 'submit_survey' };
 };
