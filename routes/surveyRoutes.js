a4eaf7f664d0491dbccab849fe4998b93036a187
Matthew Henderson <info@matthewahenderson.com>
2022-08-04 11:13:01 -0400

    Added post route for recording yes or no clicks and saving to db; modified route to account for yes or no selection

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,11 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
-
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
@@ -11,6 +13,37 @@ module.exports = app => {
         res.send('Thanks for voting!');
     });
     
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: { email: email, responded: false }
+                        }
+                    }, 
+                    {
+                        $inc: { [choice]: 1 },
+                        $set: { 'recipients.$.responded': true }
+                    }
+                ).exec();
+            })
+            .value();
+        
+        res.send({});
+    })
+
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
